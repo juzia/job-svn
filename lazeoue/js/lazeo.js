0cd159e39193dcdc4d9bef5773ece0131df5c530
juzia <5806674@qq.com>
2014-12-17 19:56:24 +0800

    【add】  院长页,新闻列表页
    layout.css  news.html  yuanzhang.html

---
+++ lazeoue/js/lazeo.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+	$(".carousel-box").jCarouselLite({
+	btnNext: ".bt-next",//下一个按钮的选择器
+	btnPrev: ".bt-prev",//上一个按钮的选择器
+	auto: null,//自动滚动
+	speed: 200,//滚动速度
+	vertical: false, //垂直滚动
+	circular: true, //循环
+	visible:1, //显示的个数
+	start: 0, //第几个开始
+	scroll: 1 //设置滚动的个数
+	});
+	});
+
+
+
+
+if($('.broadcast-in').length>0){
+	(function(){
+		$('.broadcast-in').each(function(){
+			var _this=$(this),i = 0,w=_this.find('.broadcast-con').width(),speed=50;
+			$(this).append($(this).html());
+			var bt = setInterval(function(){i--;if((0-i) >= w){i = 0;} _this.css('marginLeft',i);},speed);
+			$(this).hover(function(){clearInterval(bt)},function(){bt = setInterval(function(){i--;if((0-i) >= w){i = 0;} _this.css('marginLeft',i);},speed);})
+		});
+	})();
+};
+
+
+if($('.focus-num').length>0){
+	$(".focus-num").tabs(".focus-item", {
+			event:'mouseover',
+			effect: 'fade',
+			fadeOutSpeed: "300",
+			rotate: true
+		}).slideshow();
+		$(".focus-num").data("slideshow").play();
+}
+
+
+
+$(function() {
+	var jsToggleHead='.js-toggle-header';
+	var jsToggleBody='.js-toggle-con';
+	$(".js-toggle").delegate(jsToggleHead, "click", function() {
+	 	var $this=$(this);
+		if($this.hasClass('current'))
+		{
+			$this.next().slideUp();
+			$this.removeClass('current');
+		}
+		else{
+			$(jsToggleHead).removeClass('current');
+			$(jsToggleBody).slideUp();
+			$this.next().slideDown();
+			$this.addClass('current');
+		}
+	});
+
+})
