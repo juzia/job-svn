150de1f059e297e7df7fce2207f3a9f7f73983b2
juzia <5806674@qq.com>
2014-12-29 22:59:10 +0800

    edit
    index / css/ img

---
+++ gaodeng/js/gao.js
@@ -0,0 +1,16 @@
+
+if($('.focus-num').length>0){
+	$(".focus-num").tabs(".focus-item", {
+			event:'mouseover',
+			effect: 'fade',
+			fadeOutSpeed: "300",
+			rotate: true
+		}).slideshow();
+		$(".focus-num").data("slideshow").play();
+}
+
+
+
+$(".js-tab").each(function(){
+		$(".gb-tab:eq(0)",$(this)).tabs($(".gb-tab-pn:eq(0) > div",$(this)),{event:'mouseover'});
+	});	
